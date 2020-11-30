989525f1a27d16e18470ee2da45ce78eeed6a0e7
Stranger01 <joseantoniosysnet@gmail.com>
2020-11-30 17:25:01 -0500

    sets y listas

--- bin/learning_dart.dart
+++ bin/learning_dart.dart
@@ -1,10 +1,26 @@
-import 'dart:io';
-
 void main(List<String> arguments) {
-  stdout.write('Escribe tu nombre\r\n');
-  var name = stdin.readLineSync();
+  var test = [1, 2, 3, 4, 5, 6];
+  print(test.length);
+  print(test[0]);
 
-  name.isEmpty
-      ? stderr.write('No puedes dejarlo vacio')
-      : stdout.write('Hola ${name}');
+  print(test.elementAt(3));
+
+  var things = [];
+  things.add(1);
+  things.add('cats');
+  things.add(true);
+  print(things);
+
+  var numbers = <int>[];
+  numbers.add(1);
+  numbers.add(2);
+  numbers.add(3);
+  numbers.add(4);
+
+  print(numbers);
+
+
+  //Sets
+
+
 }
